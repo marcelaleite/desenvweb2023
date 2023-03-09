c4cd4f565a8287b45ca3b1c371f1ac0d84235c94
Marcela Leite <marcela.leite@gmail.com>
2023-03-09 15:42:05 -0300

    Atualização

--- at01/acao.php
+++ at01/acao.php
@@ -1,21 +1,28 @@
 <?php
 // depurar variáveis HTML
-echo '<pre>';
-var_dump($_POST);
-echo '</pre>';
+// echo '<pre>';
+// var_dump($_POST);
+// echo '</pre>';
  
-
+// adicionar arquivos de configuração
 require_once('config.inc.php');
+
 // abrir conexão com o banco
 $conexao = new PDO(MYSQL_DSN ,MYSQL_USUARIO, MYSQL_SENHA);
+
+//testa se a conexão deu certo
 if (!$conexao){
     echo 'Erro ao conectar';
-    die();
+    die();   // interrompe a execução do script caso ocorra erro
 }else{
-    // montar a consulta
+    // montar a consulta - aqui é somente o comando como string que será executado - as variáveis indicadas com : no inicio do nome devem ser substituídas pelos valores enviados pelo formulário 
     $consulta = 'INSERT INTO contatos (nome, sobrenome, email, dtnasci, telefone, cidade, obs, vivo ) 
                 VALUES(:nome,:sobrenome, :email, :dtnasci,:telefone, :cidade, :obs, :vivo )';
+    
+    // agora prepara o comando para execução - aqui o comando cria um 'objeto' que terá as informações do comando que será executado no BD
     $query = $conexao->prepare($consulta);
+    
+    // agora é necessário enviar os valores que serão salvos no banco, vinculando com os parâmetros da consulta
     $query->bindValue(':nome', $_POST['nome']);
     $query->bindValue(':sobrenome', $_POST['sobrenome']);
     $query->bindValue(':email', $_POST['email']);
